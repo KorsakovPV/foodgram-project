d66fa4c51d0632dc9c13cb7e068528814df47af4
Korsakov Pavel <pavelkpv@gmail.com>
2020-12-11 00:29:54 +0800

    переделываю модели карена

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from users.views import signup
 
-
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('signup/', signup, name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,22 +1,31 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
 from django.shortcuts import render
 
 from users.forms import CreationForm
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
-def signup(request):
-    context = {}
-    form = CreationForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            # return render(request, reverse('index'))
-            return HttpResponseRedirect(reverse(('index')))
-    context['form'] = form
-    return render(request, 'registration/authForm.html', context)
+# def signup(request):
+#     context = {}
+#     form = CreationForm(request.POST or None)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             # return render(request, reverse('index'))
+#             return HttpResponseRedirect(reverse(('index')))
+#     context['form'] = form
+#     return render(request, 'registration/signup.html', context)
+
+
+
+
+class signup(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
